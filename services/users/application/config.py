2109a0683f7c5fd7da0b0efc3c7eb3edd99cf7be
juancece <juancece@outlook.com>
2020-09-13 22:35:35 -0500

    fix for docker and application

---
+++ services/users/application/config.py
@@ -0,0 +1,18 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """ Base Configuration """
+
+class ProductionConfig(Config):
+    """ Production Configuration """
+
+class DevelopmentConfig(Config):
+    """ Development Configuration """
+
+class TestingConfig(Config):
+    """ Testing Configuration """
+    TESTING = True
+
